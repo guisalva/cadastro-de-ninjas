0144d8b8f226e64901c96e5c8c6cce2f0cf18673
Guilherme Salvador <guilherme@cobrecom.com.br>
2026-08-24 14:20:51 -0300

    Cria o controller

---
+++ src/main/java/br/com/guilhermesalvador/cadastrodeninjas/Controller.java
@@ -0,0 +1,16 @@
+package br.com.guilhermesalvador.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boas-vindas")
+    public String boasVindas() {
+        return "Essa é a primeira mensagem nessa rota";
+    }
+
+}
