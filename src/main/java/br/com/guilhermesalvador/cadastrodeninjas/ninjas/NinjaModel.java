9220d0211ab20882c4cc4a9a92ba90d12e76b231
Guilherme Salvador <guilherme@cobrecom.com.br>
2026-08-25 14:21:45 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaModel.java
+++ src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaModel.java
@@ -2,55 +2,32 @@ package br.com.guilhermesalvador.cadastrodeninjas.ninjas;
 
 import br.com.guilhermesalvador.cadastrodeninjas.missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // Entity transforma uma classe em uma entidade do Banco de Dados
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
     // @ManyToOne - "muitos" ninjas estao relacionados a "uma" missao
     @ManyToOne
     @JoinColumn(name = "missao_id") // Foreign Key
     private MissaoModel missao;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
