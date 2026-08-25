dc74699958192c9db92801d677eac988058aba40
Guilherme Salvador <guilherme@cobrecom.com.br>
2026-08-25 16:23:50 -0300

    feat: Adiciona arquivos relacionados a arquitetura layered

---
+++ src/main/java/br/com/guilhermesalvador/cadastrodeninjas/missoes/MissaoRepository.java
@@ -0,0 +1,6 @@
+package br.com.guilhermesalvador.cadastrodeninjas.missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
