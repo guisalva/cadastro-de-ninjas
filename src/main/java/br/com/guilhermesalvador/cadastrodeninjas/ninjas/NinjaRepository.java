3707fcf06b8668b11899f6b02831b402493279dc
Guilherme Salvador <guilherme@cobrecom.com.br>
2026-08-27 09:56:27 -0300

    build: Conecta o banco de dados H2 com a aplicação Spring

--- src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaRepository.java
+++ src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package br.com.guilhermesalvador.cadastrodeninjas.ninjas;
 
-public interface NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
