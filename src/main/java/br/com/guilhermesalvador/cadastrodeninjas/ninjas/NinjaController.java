6f2aabb715a66f348545fbbafc540b7bd8c20100
Guilherme Salvador <guilherme@cobrecom.com.br>
2026-08-31 13:35:46 -0300

    feat: Altera rota do controller para listar os ninjas e configura o NinjaService para utilizar o NinjaRepository

--- src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaController.java
+++ src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaController.java
@@ -2,9 +2,17 @@ package br.com.guilhermesalvador.cadastrodeninjas.ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boas-vindas")
     public String boasVindas() {
@@ -19,8 +27,8 @@ public class NinjaController {
 
     // Mostrar todos os Ninjas (READ)
     @GetMapping("/listar")
-    public String listarNinjas() {
-        return "Ninjas";
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinjas();
     }
 
     // Mostrar Ninja por ID (READ)
--- src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaService.java
+++ src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaService.java
@@ -1,4 +1,20 @@
 package br.com.guilhermesalvador.cadastrodeninjas.ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // Listar os ninjas
+    public List<NinjaModel> listarNinjas() {
+        return ninjaRepository.findAll();
+    }
 }
