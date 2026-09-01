bbf50d5475a017e0610815ea70e1c24edd515824
Guilherme Salvador <guilherme@cobrecom.com.br>
2026-09-01 10:35:19 -0300

    feat: Inclui rota para deletar um ninja

--- src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaService.java
+++ src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaService.java
@@ -30,4 +30,9 @@ public class NinjaService {
     public NinjaModel criarNinja(NinjaModel ninja) {
         return ninjaRepository.save(ninja);
     }
+
+    // Deletar um Ninja
+    public void deletarNinjaPorId(Long id) {
+        ninjaRepository.deleteById(id);
+    }
 }
