9231a6f749d96cbb10ddfe8d88ede771da654c8e
Guilherme Salvador <guilherme@cobrecom.com.br>
2026-09-01 09:33:17 -0300

    feat: Inclui rota para criar novos ninjas

--- src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaService.java
+++ src/main/java/br/com/guilhermesalvador/cadastrodeninjas/ninjas/NinjaService.java
@@ -25,4 +25,9 @@ public class NinjaService {
 
         return ninja.orElse(null);
     }
+
+    // Criar um novo Ninja
+    public NinjaModel criarNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
 }
